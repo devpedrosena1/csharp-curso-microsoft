c45f6254245834e98005e5732bb5831374359e5a
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-14 19:38:20 -0300

    criando projeto para criar exemplos de operadores condicionais em c#

---
+++ CursoMicrosoftCSharp/OperadorCondicional/Program.cs
@@ -0,0 +1,5 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+// <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
