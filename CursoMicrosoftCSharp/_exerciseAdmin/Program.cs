989033b700f00a63b863d44e31d18754464d1597
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-14 20:01:52 -0300

    realizando exercicicio de operadores ternarios

---
+++ CursoMicrosoftCSharp/_exerciseAdmin/Program.cs
@@ -0,0 +1,21 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var permission = "Admin|Manager";
+var level = 55;
+
+var result1 = permission.Contains("Admin") && level > 55 ? "Welcome, Super Admin user." : "Welcome.";
+Console.WriteLine($"User:  {result1}");
+
+var result2 = permission.Contains("Admin") && level <= 55 ? "Welcome, Admin user." : "Welcome.";
+Console.WriteLine($"User:  {result2}");
+
+var result3 = permission.Contains("Manager") && level >= 20 ? "Contact an Admin for access." : "Welcome.";
+Console.WriteLine($"User:  {result3}");
+
+var result4 = permission.Contains("Manager") && level < 20 ? "You do not have sufficient privileges." : "Welcome.";
+Console.WriteLine($"User:  {result3}");
+
+var result5 = permission.Contains("User") && level < 20 ? "You do not have sufficient privileges." : "Welcome.";
+Console.WriteLine($"User:  {result3}");
