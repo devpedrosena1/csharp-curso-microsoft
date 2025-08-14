41819d1dc3b3346d7a4e8374e4234f50685aad0a
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-14 19:41:29 -0300

    entendendo operadorees condicionais

--- CursoMicrosoftCSharp/OperadorCondicional/Program.cs
+++ CursoMicrosoftCSharp/OperadorCondicional/Program.cs
@@ -3,3 +3,7 @@
 Console.WriteLine("Hello, World!");
 
 // <evaluate this condition> ? <if condition is true, return this value> : <if condition is false, return this value>
+
+var saleAmout = 1001;
+var discount = saleAmout > 1000 ? 100 : 50;
+Console.WriteLine($"Discount is {discount}");
