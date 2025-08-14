413d776a0ade39bf23a399f348974f208b20cb3b
devpedrosena1 <devpedrosena1@gmail.com>
2025-08-14 19:49:12 -0300

    realizando primeiro exercicio sobre operadores condicionais e operadores ternarios em c#

---
+++ CursoMicrosoftCSharp/_exerciseOperatorConditional/Program.cs
@@ -0,0 +1,7 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+Console.WriteLine("Hello, World!");
+
+var coin = new Random();
+var flip = coin.Next(0, 2);
+Console.WriteLine((flip == 0) ? "Heads" : "Tails");
